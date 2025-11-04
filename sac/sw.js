c63a24cb48c768e74f29e467006bfa8f2f7d04ac
Nick Clifford <nclifford@gocardless.com>
2025-11-04 23:31:45 +0200

    Add files via upload

---
+++ sac/sw.js
@@ -0,0 +1,25 @@
+/* Offline cache for SAC Calculator (GitHub Pages compatible) */
+const CACHE = 'sac-pwa-final-v1';
+const ASSETS = ['./', './index.html', './icon-192.png', './icon-512.png', './manifest.webmanifest'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.map((k) => k !== CACHE ? caches.delete(k) : null)))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request).then((resp) => {
+      const copy = resp.clone();
+      caches.open(CACHE).then((cache) => cache.put(event.request, copy)).catch(() => {});
+      return resp;
+    }).catch(() => caches.match('./index.html')))
+  );
+});
